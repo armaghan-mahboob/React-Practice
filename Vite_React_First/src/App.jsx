eee56221bbdbc3e41f8d07284704a94369b8a39a
ManiTheLegend <cute.armanijeans@gmail.com>
2026-04-11 22:36:51 +0500

    Created my first Vite + React with Tailwind project

---
+++ Vite_React_First/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-yellow-500 text-4xl">
+        React + Vite + Tailwind first project
+      </h1>
+    </div>
+  );
+}
+
+export default App;
