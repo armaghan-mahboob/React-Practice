338d289a9b8145963574dbe21882ebc9ffc74d13
ManiTheLegend <cute.armanijeans@gmail.com>
2026-06-15 14:33:50 +0500

    todos state persisted in localStorage

--- Todo/src/store/store.js
+++ Todo/src/store/store.js
@@ -1,5 +1,32 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import todoReducer from "./todoSlice";
+
+// const store = configureStore({ reducer: { todo: todoReducer } });
+// export default store;
+
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
 
-const store = configureStore({ reducer: { todo: todoReducer } });
+const loadState = () => {
+  try {
+    const serialized = localStorage.getItem("todos");
+    return serialized ? { todo: JSON.parse(serialized) } : undefined;
+  } catch {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    localStorage.setItem("todos", JSON.stringify(state.todo));
+  } catch {}
+};
+
+const store = configureStore({
+  reducer: { todo: todoReducer },
+  preloadedState: loadState(),
+});
+
+store.subscribe(() => saveState(store.getState()));
+
 export default store;
